1330ae6ef5447bd92e36e4a96d36128eaba322ee
Deepanker005 <Deepanker2k19@gmail.com>
2024-01-27 21:11:12 +0530

    Did Url rerouting and views setup with HttpResponse

--- amazon_clone/base/views.py
+++ amazon_clone/base/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+    return HttpResponse("This is my Home Page")
 
 # Create your views here.
